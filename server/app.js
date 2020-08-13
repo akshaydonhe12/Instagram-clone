3674594a3a3e7bdae1321da039339f36f37d973c
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-08-13 11:31:55 +0530

    nodemon install

--- server/app.js
+++ server/app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+const customMiddleware = (req, res, next) => {
+    console.log('middleware work')
+    next()
+}
+
+app.use(customMiddleware)
+
 app.get('/',(req, res)=> {
     res.send('Hello World')
 })
